4d06f486e0570b202d4be4949dfeafad184252e9
AnxietyisReal <96593068+AnxietyisReal@users.noreply.github.com>
2023-05-14 18:41:15 +1000

    Did a test, gave up with it due to time.

--- ampapi.ts
+++ ampapi.ts
@@ -1,69 +1,66 @@
 interface Data {SESSIONID:string}
-import axios from 'axios';
-export default function(baseURL:string){
-  const self = this;
-  this.baseURL = baseURL;
-  this.sessionId = '';
-  this.dataSource = '';
-  this.API = {
-    Core: {
-      GetAPISpec: []
-    }
-  };
 
-  this.initAsync = async function(stage2:boolean){
-    this.dataSource = (baseURL.endsWith('/') ? baseURL.substring(0,baseURL.lastIndexOf('/')) : baseURL)+'/API';
-    
-    for(const module of Object.keys(this.API)){
-      const methods = this.API[module];
-      this[module] = {};
+class ampapi {
+  sessionId = ''
+  dataSource = ''
+  API = { Core: { GetAPISpec: [] } }
+}
 
-      for(const method of Object.keys(methods)){
-        this[module][method+'Async'] = function(params){
-          const args = Array.prototype.slice.call(arguments,0);
-          return self.APICall(module,method,args);
+class AMPAPI {
+  initAsync:any
+  APICall:any
+
+  constructor(baseURL:string){
+    const calls = new ampapi();
+    this.initAsync = async function(stage2:boolean){
+      calls.dataSource = (baseURL.endsWith('/') ? baseURL.substring(0,baseURL.lastIndexOf('/')) : baseURL)+'/API';
+      for(const module of Object.keys(calls.API)){
+        const methods = calls.API[module];
+        this[module] = {};
+        
+        for(const method of Object.keys(methods)){
+          this[module][method+'Async'] = function(params:string){
+            const args = Array.prototype.slice.call(arguments,0);
+            return this.APICall(module,method,args)
+          }
+        }
+
+        if (stage2) return true;
+        else {
+          const result = await this.Core.GetAPISpecAsync();
+          if (result != null){
+            calls.API = result;
+            return await this.initAsync(true)
+          } else return false
         }
       }
+    }
 
-      if (stage2) return true;
-      else {
-        const result = await this.Core.GetAPISpecAsync();
-        if (result != null){
-          this.API = result;
-          return await this.initAsync(true);
-        } else return false;
+    this.APICall = async function(module:string,methodName:string,args:string){
+      const data = {} as Data;
+      const methodParams = this.API[module][methodName].Parameters;
+      const methodParamsLength = methodParams != null ? methodParams.length : 0;
+
+      for(var a = 0; a < methodParamsLength; a++){
+        const argName = methodParams[0].Name;
+        const val = args[a];
+        data[argName] = val;
+      }
+      
+      const URI = `${calls.dataSource}/${module}/${methodName}`;
+      data.SESSIONID = calls.sessionId;
+      try{
+        const x = await fetch(URI,{
+          method: 'POST',
+          headers: {'Accept':'application/vnd.cubecoders-ampapi','User-Agent':'AMPAPI-NodeJS'},
+          body: JSON.stringify(data)
+        });
+        return x.body
+      }catch(err){
+        throw new Error(`Failed to make a POST request to "${calls.dataSource}"`, {cause: err})
       }
     }
-  }
-
-  this.APICall = function(module:string,methodName:string,args:string){
-    const data = {} as Data;
-    
-    const methodParams = this.API[module][methodName].Parameters;
-    const methodParamsLength = methodParams != null ? methodParams.length : 0;
-
-    for(var a = 0; a < methodParamsLength; a++){
-      const argName = methodParams[a].Name;
-      const val = args[a];
-      data[argName] = val;
-    }
-
-    const URI = `${this.dataSource}/${module}/${methodName}`;
-    data.SESSIONID = this.sessionId;
-
-    return new Promise(function(resolve,reject){
-      axios.post(URI,{
-        body: JSON.stringify(data)
-      },{
-        headers:{'Accept':'application/vnd.cubecoders-ampapi','User-Agent':`AMPAPI-NodeJS/Axios ${axios.VERSION}`}
-      }).then(()=>function(error:any,res:any,body:any){
-        if (error) reject(error);
-        else if (!error){
-          const result = JSON.parse(body);
-          if (result != null && Object.keys(result).length === 1 && result.result !== undefined) resolve(result.result);
-          else resolve(result)
-        }
-      })
-    })
   }
 }
+
+export default AMPAPI
